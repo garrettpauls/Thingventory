115904a45421a1b709063b3a8f0631e9f2d0c250
Garrett Pauls <garrett.pauls@gmail.com>
2017-12-07 19:51:31 -0700

    Added EFCore models without migrations

---
+++ Source/Thingventory/Models/Inventory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Thingventory.Models
+{
+    [DataContract(Name = "Inventory", Namespace = "Thingventory")]
+    public sealed class Inventory : ChangeTrackingModel
+    {
+        private string mName;
+
+        public Inventory(Guid id)
+        {
+            Id = id;
+        }
+
+        [DataMember(Name = "Id")]
+        public Guid Id { get; }
+
+        [DataMember(Name = "Name")]
+        public string Name
+        {
+            get => mName;
+            set => Set(ref mName, value);
+        }
+    }
+}
---
+++ Source/Thingventory/Services/Data/ThingDataContext.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using Windows.Storage;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Thingventory.Models;
+
+namespace Thingventory.Services.Data
+{
+    public sealed class ThingDataContext : DbContext
+    {
+        private readonly string mFile;
+
+        public ThingDataContext() : this("")
+        {
+        }
+
+        public ThingDataContext(string rootPath)
+        {
+            mFile = Path.Combine(rootPath, "data.sqlite");
+        }
+
+        public DbSet<LocationEntity> Locations { get; set; }
+
+        public static ThingDataContext Create(Inventory inventory)
+        {
+            var rootPath = Path.Combine(
+                ApplicationData.Current.LocalFolder.Path,
+                "Inventories",
+                inventory.Id.ToString("N"));
+            return new ThingDataContext(rootPath);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connection = new SqliteConnectionStringBuilder();
+            connection.DataSource = mFile;
+
+            optionsBuilder.UseSqlite(connection.ToString());
+        }
+    }
+}
