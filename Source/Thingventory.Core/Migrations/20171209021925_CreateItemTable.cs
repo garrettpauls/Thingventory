a8401baac7d2aba01eaa43f42fa6c33556c75d98
Garrett Pauls <garrett.pauls@gmail.com>
2017-12-08 19:21:35 -0700

    Added migration for item table

---
+++ Source/Thingventory.Core/Migrations/20171209021925_CreateItemTable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Thingventory.Core.Migrations
+{
+    public partial class CreateItemTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Item",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CreatedInstant = table.Column<DateTimeOffset>(nullable: false),
+                    LocationId = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    UpdatedInstant = table.Column<DateTimeOffset>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Item", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Item");
+        }
+    }
+}
